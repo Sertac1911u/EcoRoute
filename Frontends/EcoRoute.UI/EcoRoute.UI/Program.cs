b0dd06f2dd56d1faa24ad3f3f9027b4d4c1b41fe
Sertac1911u <114613689+Sertac1911u@users.noreply.github.com>
2025-03-25 18:09:04 +0300

    update

---
+++ Frontends/EcoRoute.UI/EcoRoute.UI/Program.cs
@@ -0,0 +1,45 @@
+﻿using EcoRoute.UI.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Servislerin eklenmesi
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+
+// Cookie tabanlı authentication ayarları
+builder.Services.AddAuthentication("MyCookieScheme")
+    .AddCookie("MyCookieScheme", options =>
+    {
+        options.LoginPath = "/login";
+        options.LogoutPath = "/logout";
+        options.AccessDeniedPath = "/access-denied";
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// HTTP istek hattının yapılandırılması
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// Routing middleware’inin eklenmesi
+app.UseRouting();
+
+// Authentication ve Authorization middleware’larının eklenmesi
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Blazor Server endpoint’lerinin eşlenmesi
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
